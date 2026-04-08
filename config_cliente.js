1d6615798f433e387d880bf8ca4652526fb05396
chayonet <chayocc2026@gmail.com>
2026-04-08 00:17:17 -0500

    Update config_cliente.js

--- config_cliente.js
+++ config_cliente.js
@@ -1,5 +1,6 @@
 /* ==========================================
    ARCHIVO: config.js
-   Variable global para conectar toda la app
+   Variables globales para personalizar la app
 ========================================== */
+const NOMBRE_NEGOCIO = "CHAYONET"; // <--- Solo cambias esto y listo el pollo
 const API_BASE_URL_CLIENTE = "https://apis-yley.onrender.com";
